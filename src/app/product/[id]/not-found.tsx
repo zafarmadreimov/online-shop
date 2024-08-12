574375c00871790ac2c7a052451c382b4f7109a3
Zafarbek <madreimovzafarbek2@gmail.com>
2024-08-12 19:38:16 +0500

    online-shop

---
+++ src/app/product/[id]/not-found.tsx
@@ -0,0 +1,12 @@
+
+
+const NotFound = () => {
+  return (
+   <div className="mt-24">
+    <h2>Not Found</h2>
+    <p>Could not find requested resource</p>
+   </div>
+  )
+}
+
+export default NotFound
