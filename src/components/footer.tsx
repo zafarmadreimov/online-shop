5b9a45372f98373d7a4cdc1d22532a69e06544ea
Zafar <madraimovzafarbek2@gmail.com>
2024-08-15 20:11:21 +0500

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -15,14 +15,14 @@ const Footer = () => {
 					/>
 				</Link>
 				<p className='text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4'>
-					© 2020 online shop —
+					© 2024 online shop —
 					<a
 						href='https://twitter.com/knyttneve'
 						className='text-gray-600 ml-1'
 						rel='noopener noreferrer'
 						target='_blank'
 					>
-						@samarbadriddinov
+						@madreimovzafarbek
 					</a>
 				</p>
 				<span className='inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start'>
